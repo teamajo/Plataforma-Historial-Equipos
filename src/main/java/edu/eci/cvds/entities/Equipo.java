4d87836c3c3c1a755af60cae59f090544a9932ce
Juan Ospina <juan.ospina-h@mail.escuelaing.edu.co>
2019-04-22 20:12:11 -0500

    prueba registro

--- src/main/java/edu/eci/cvds/entities/Equipo.java
+++ src/main/java/edu/eci/cvds/entities/Equipo.java
@@ -1,20 +1,28 @@
 package edu.eci.cvds.entities;
 
+import java.util.ArrayList;
 
 
 /**
- * Clase que representa un elemento 
+ * Clase que representa un elemento
  */
 
  public class Equipo {
     private int id;
     private String lab;
+    private Elemento torre;
+    private Elemento pantalla;
+    private Elemento mouse;
+    private Elemento teclado;
 
-    public Equipo(int id, String lab) {
+    public Equipo(int id, String lab, Elemento torre, Elemento pantalla, Elemento mouse, Elemento teclado) {
         this.id = id;
         this.lab = lab;
+        this.setTorre(torre);
+        this.setPantalla(pantalla);
+        this.setMouse(mouse);
+        this.setTeclado(teclado);
     }
-
 
     public Equipo() {
         super();
@@ -50,6 +58,63 @@ package edu.eci.cvds.entities;
      */
     public void setLab(String lab) {
         this.lab = lab;
+    }
+
+    
+    /**
+     * @return the teclado
+     */
+    public Elemento getTeclado() {
+        return teclado;
+    }
+
+    /**
+     * @param teclado the teclado to set
+     */
+    public void setTeclado(Elemento teclado) {
+        this.teclado = teclado;
+    }
+
+    /**
+     * @return the mouse
+     */
+    public Elemento getMouse() {
+        return mouse;
+    }
+
+    /**
+     * @param mouse the mouse to set
+     */
+    public void setMouse(Elemento mouse) {
+        this.mouse = mouse;
+    }
+
+    /**
+     * @return the pantalla
+     */
+    public Elemento getPantalla() {
+        return pantalla;
+    }
+
+    /**
+     * @param pantalla the pantalla to set
+     */
+    public void setPantalla(Elemento pantalla) {
+        this.pantalla = pantalla;
+    }
+
+    /**
+     * @return the torre
+     */
+    public Elemento getTorre() {
+        return torre;
+    }
+
+    /**
+     * @param torre the torre to set
+     */
+    public void setTorre(Elemento torre) {
+        this.torre = torre;
     }
 
     
--- src/main/java/edu/eci/cvds/managedbeans/AgregarEquipoBean.java
+++ src/main/java/edu/eci/cvds/managedbeans/AgregarEquipoBean.java
@@ -10,6 +10,7 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 
+import edu.eci.cvds.entities.Elemento;
 import edu.eci.cvds.entities.Equipo;
 import edu.eci.cvds.services.LaboratorioServices;
 import edu.eci.cvds.services.ServicesException;
@@ -27,11 +28,16 @@ public class AgregarEquipoBean extends BasePageBean {
     private LaboratorioServicesImpl laboratorioServices;
 	
     private Equipo nuevoEquipo;
+    private Elemento torre;
+    private Elemento pantalla;
+    private Elemento mouse;
+    private Elemento teclado;
 
-
-    public AgregarEquipoBean(){
-        nuevoEquipo= new Equipo();
+    public AgregarEquipoBean() {
+        nuevoEquipo = new Equipo();
     }
+
+ 
 
     public Equipo getNuevoEquipo() {
         return nuevoEquipo;
@@ -43,6 +49,22 @@ public class AgregarEquipoBean extends BasePageBean {
 
     public void registrarEquipo() throws Exception {
         String mensaje;
+        List<Elemento> disponibles = null;
+        disponibles = laboratorioServices.elementosDisponibles();
+        List<Elemento> elementos = null;
+        elementos = crearList();
+        for (int i = 0; i< elementos.size();i++){
+            int prueba = estaDisponible(elementos.get(i), disponibles);
+            if(prueba == 2){
+                laboratorioServices.asociarEquipo(elementos.get(i).getId(), nuevoEquipo.getId());
+            }else if (prueba == 1 ){
+                laboratorioServices.registrarElemento(elementos.get(i));
+                laboratorioServices.asociarEquipo(elementos.get(i).getId(), nuevoEquipo.getId());
+            }else{
+                mensaje="No se pudo registar el elemento " + elementos.get(i)+"ya que se encuentra en otro elemento";
+                FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,mensaje,mensaje));
+            }
+        }
         try {
             laboratorioServices.registrarEquipo(nuevoEquipo);
             mensaje = "success !!";
@@ -52,7 +74,90 @@ public class AgregarEquipoBean extends BasePageBean {
         }
         FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,mensaje,mensaje));
     }
+    public List<Elemento> crearList(){
+        ArrayList<Elemento> elementos = new ArrayList<Elemento>();
+        elementos.add(torre);
+        elementos.add(pantalla);
+        elementos.add(mouse);
+        elementos.add(teclado);
+        return elementos;
+    }
+
+    public int estaDisponible(Elemento elemento, List<Elemento> disponibles) throws Exception {
+        int flag = 0;
+        try {
+            if (disponibles.contains(elemento) ){
+                flag = 2;
+            }else{
+                if (!laboratorioServices.buscarElementos().contains(elemento)){
+                       flag=1; 
+                }else{
+                    flag=0;
+                }
+            }
+        } catch (ServicesException ex) {
+            throw ex;
+        }
+        return flag;
+    }
+
      
+
+    /**
+     * @return the mouse
+     */
+    public Elemento getMouse() {
+        return mouse;
+    }
+
+    /**
+     * @param mouse the mouse to set
+     */
+    public void setMouse(Elemento mouse) {
+        this.mouse = mouse;
+    }
+
+    /**
+     * @return the teclado
+     */
+    public Elemento getTeclado() {
+        return teclado;
+    }
+
+    /**
+     * @param teclado the teclado to set
+     */
+    public void setTeclado(Elemento teclado) {
+        this.teclado = teclado;
+    }
+
+    /**
+     * @return the pantalla
+     */
+    public Elemento getPantalla() {
+        return pantalla;
+    }
+
+    /**
+     * @param pantalla the pantalla to set
+     */
+    public void setPantalla(Elemento pantalla) {
+        this.pantalla = pantalla;
+    }
+
+    /**
+     * @return the torre
+     */
+    public Elemento getTorre() {
+        return torre;
+    }
+
+    /**
+     * @param torre the torre to set
+     */
+    public void setTorre(Elemento torre) {
+        this.torre = torre;
+    }
 	
 
 }
